14845f10f3adf772ec6d405b3306394a2441b2aa
DRAGONTOS <koningdragon@gmail.com>
2022-12-02 23:25:16 +0100

    awdwwq

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,12 +1,12 @@
 static const Block blocks[] = {
-	{"",	"/home/rander/.config/suckless/dwmscripts/dwmweather", 	3600,	0},
-	{"",	"/home/rander/.config/suckless/dwmscripts/dwmcpu", 	5,	0},
-	{"",	"/home/rander/.config/suckless/dwmscripts/dwmmemory", 	10,	0},
-	{"",	"/home/rander/.config/suckless/dwmscripts/dwmbatt", 	5,	0},
-	{"",	"/home/rander/.config/suckless/dwmscripts/dwmtemp", 	10,	0},
-	{"",	"/home/rander/.config/suckless/dwmscripts/dwmdisk", 	3600,	0},
-	{"",	"/home/rander/.config/suckless/dwmscripts/dwmupdates", 	3600,	0},
-	{"",	"/home/rander/.config/suckless/dwmscripts/dwmclock", 	60,	0},
+	{"",	"/home/rander/.config/Dwm-Setup/dwmscripts/dwmweather", 	3600,	0},
+	{"",	"/home/rander/.config/Dwm-Setup/dwmscripts/dwmcpu", 	5,	0},
+	{"",	"/home/rander/.config/Dwm-Setup/dwmscripts/dwmmemory", 	10,	0},
+	{"",	"/home/rander/.config/Dwm-Setup/dwmscripts/dwmbatt", 	5,	0},
+	{"",	"/home/rander/.config/Dwm-Setup/dwmscripts/dwmtemp", 	10,	0},
+	{"",	"/home/rander/.config/Dwm-Setup/dwmscripts/dwmdisk", 	3600,	0},
+	{"",	"/home/rander/.config/Dwm-Setup/dwmscripts/dwmupdates", 	3600,	0},
+	{"",	"/home/rander/.config/Dwm-Setup/dwmscripts/dwmclock", 	60,	0},
 };
 
 static char delim[] = " | ";
